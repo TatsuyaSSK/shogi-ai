eee0dc20cbb9c3a2a7cdc6cc93280507611f7dfd
工藤　脩人 <kudou@MacBook-Air.local>
2022-01-29 15:54:55 +0900

    [change]モデルの構成と重みを変更

--- serving/app.py
+++ serving/app.py
@@ -3,18 +3,14 @@ import numpy as np
 from google.cloud import storage
 from cshogi import CSA, Board
 import torch
-from features import FEATURES_NUM, make_input_features
-from policy_value_resnet import PolicyValueNetwork
-from features import (
-    FEATURES_NUM,
-    make_input_features,
-)
+from policy_value_network_resnet10_swish import PolicyValueNetwork
+from cshogi.dlshogi import make_input_features, FEATURES1_NUM, FEATURES2_NUM
 from flask import Flask, request, Response, jsonify
 
 model = PolicyValueNetwork()
 device = torch.device("cpu")
 model.to(device)
-model_weight = torch.load("checkpoints_checkpoint-004.pth", map_location=device)
+model_weight = torch.load("checkpoint_resnet10_swish-072.pth", map_location=device)
 model.load_state_dict(model_weight["model"])
 model.eval()
 
@@ -52,21 +48,27 @@ def predict():
     board = Board()
     move_num = len(kif.moves)
     # 出力用の配列
-    torch_features = torch.empty(
-        (move_num + 1, FEATURES_NUM, 9, 9),
+    torch_features_1 = torch.empty(
+        (move_num + 1, FEATURES1_NUM, 9, 9),
+        dtype=torch.float32,
+        device=torch.device(device),
+    )
+    torch_features_2 = torch.empty(
+        (move_num + 1, FEATURES2_NUM, 9, 9),
         dtype=torch.float32,
         device=torch.device(device),
     )
     # make_input_featuresがnumpyでしか動作しないのでnumpy型に変換
-    np_features = torch_features.numpy()
+    np_features_1 = torch_features_1.numpy()
+    np_features_2 = torch_features_2.numpy()
     # 初期局面の特徴量を生成
-    make_input_features(board, np_features[0])
+    make_input_features(board, np_features_1[0], np_features_2[0])
     # 1手目から終局までの特徴量を生成
     for i, move in enumerate(kif.moves):
         board.push(move)
-        make_input_features(board, np_features[i + 1])
+        make_input_features(board, np_features1[i + 1], np_features_2[i + 1])
 
-    _, y = model(torch_features)
+    _, y = model(torch_features_1, torch_features_2)
     # Tensorをnumpyに変換
     y = y.to("cpu").detach().numpy().copy()
     # 次元を削減
